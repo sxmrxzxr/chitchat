2ac608ead983cbec69b3ef92eddc0181ea0879bc
Jimmy Klein <jimmy.klein4@gmail.com>
2016-05-18 17:10:08 -0400

    Moved components back to their own files

--- public/js/main.jsx
+++ public/js/main.jsx
@@ -1,67 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Participants = require('./participants.jsx');
+var InlineBlock = require('./inlineBlock.jsx');
+
 var serverBaseUrl = document.domain;
 var socket = io.connect(serverBaseUrl);
 var sessionId = '';
 
-var Participants = React.createClass({
-  render:function(){
-    console.log(this.props.data);
-    if(this.props.data.participants!=null) {
-      var participantNodes = this.props.data.participants.map(function(user){
-        return (
-          <p>{user.name}</p>
-        );
-      });
-    } else {
-      var participantNodes = <p></p>
-    }
-    return(
-      <div className="inlineBlock topAligned">
-        <b>Participants</b>
-        <br />
-        <div id="participants">
-          {participantNodes}
-          <br/>
-        </div>
-      </div>
-    );
-  }
-});
+//TODO: Moved messages into this component
 
-var NameField = React.createClass({
-  render: function(){
-    return(
-      <span>
-        Your Name:
-        <input type="text" value="Anonymous" id="name" />
-      </span>
-    );
-  }
-});
-
-var MessageForm = React.createClass({
-  render: function(){
-    return(
-      <form id="messageForm">
-        <textarea rows="4" cols="50" placeholder="share something" maxLength="200" id="outgoingMessage"></textarea>
-        <input type="button" value="Share" disabled id="send" />
-      </form>
-    );
-  }
-});
-
-var InlineBlock = React.createClass({
-  render: function(){
-    return(
-      <div className="inlineBlock">
-        <NameField />
-        <br />
-        <MessageForm />
-      </div>
-    );
-  }
-});
 
 var MainBody = React.createClass({
   getInitialState: function() {
@@ -79,26 +26,28 @@ var MainBody = React.createClass({
     socket.on('newConnection', function(data) {
       this.setState({data:data});
     }.bind(this));
+    //TODO
     socket.on('userDisconnected', function(data) {
       console.log(data);
     });
+    //TODO
     socket.on('nameChanged', function(data) {
       console.log(data);
     });
+    //TODO
     socket.on('incomingMessage', function(data) {
       console.log(data);
     });
+    //TODO
     socket.on('error', function(reason){
       console.log(reason);
     });
   },
   render: function(){
-    //var data=[];
     return(
       <div>
         <h1>ChitChat</h1>
         <InlineBlock />
-        {console.log(this.state.data)}
         <Participants data={this.state.data}/>
       </div>
     );
---
+++ public/js/participants.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render:function(){
+    console.log(this.props.data);
+    if(this.props.data.participants!=null) {
+      var participantNodes = this.props.data.participants.map(function(user){
+        return (
+          <li key={user.id}>{user.name}</li>
+        );
+      });
+    } else {
+      var participantNodes = <p></p>
+    }
+    return(
+      <div className="inlineBlock topAligned">
+        <b>Participants</b>
+        <br />
+        <div id="participants">
+          <ul>
+            {participantNodes}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+});
