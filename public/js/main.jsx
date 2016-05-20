e6effbf1e61ebecc468f648037e40172dbc24190
Jimmy Klein <jimmy.klein4@gmail.com>
2016-05-19 23:46:06 -0400

    Moved everything over to react

--- public/js/main.jsx
+++ public/js/main.jsx
@@ -1,12 +1,80 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Participants = require('./participants.jsx');
-var InlineBlock = require('./inlineBlock.jsx');
 
 var serverBaseUrl = document.domain;
 var socket = io.connect(serverBaseUrl);
 var sessionId = '';
 //TODO: Moved messages into this component
+
+
+var InlineBlock = React.createClass({
+  getInitialState: function() {
+    return{ name: 'Anonymous', text: '' };
+  },
+  handleTextChange: function(event) {
+    this.setState({text: event.target.value});
+  },
+  handleNameChange: function(event){
+    var n = event.target.value;
+    var id = sessionId;
+    socket.emit('nameChange', {
+      id: id,
+      name: n
+    });
+    this.setState({name: event.target.value});
+  },
+  handleSubmit: function(event) {
+    event.preventDefault();
+    var name = this.state.name;
+    var text = this.state.text;
+    $.ajax({
+      url: '/message',
+      type: 'POST',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        message: text,
+        name: name
+      })
+    });
+    ReactDOM.findDOMNode(this.refs.message).value = '';
+  },
+  handleKeyPress: function(event) {
+    if(event.key === 'Enter') {
+      this.handleSubmit(event);
+    }
+  },
+  render: function(){
+    return(
+      <div className="inlineBlock">
+        <span>
+          Your Name:
+          <input
+            type="text"
+            value={this.state.name}
+            id="name"
+            onChange={this.handleNameChange}
+            />
+        </span>
+        <br />
+        <form id="messageForm" onSubmit={this.handleSubmit}>
+          <textarea
+            rows="4"
+            cols="50"
+            placeholder="share something"
+            maxLength="200"
+            id="outgoingMessage"
+            ref="message"
+            onKeyPress = {this.handleKeyPress}
+            onChange={this.handleTextChange}>
+          </textarea>
+          <input type="submit" value="Share" />
+        </form>
+      </div>
+    );
+  }
+});
 
 
 var Messages = React.createClass({
@@ -24,7 +92,7 @@ var Messages = React.createClass({
   render: function() {
     var commentNodes = this.state.data.map(function(e) {
       return(
-        <p><b>{e.name}</b> {e.message} <br/></p>
+        <p><b>{e.name}</b> {e.message} </p>
       );
     });
     return(
